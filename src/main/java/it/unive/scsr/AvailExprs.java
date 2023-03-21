805a0f7e2fe4a0591ae28b5e7ffe6a0aef085af4
Luca Negrini <luca.negrini@unive.it>
2023-03-21 12:07:52 +0100

    Skeleton of the available expressions class

---
+++ src/main/java/it/unive/scsr/AvailExprs.java
@@ -0,0 +1,28 @@
+package it.unive.scsr;
+
+public class AvailExprs {
+
+	// IMPLEMENTATION NOTE:
+	// the code below is outside of the scope of the course. You can uncomment
+	// it to get your code to compile. Be aware that the code is written
+	// expecting that a field named "expression" of type ValueExpression exists
+	// in this class: if you name it differently, change also the code below to
+	// make it work by just using the name of your choice instead of
+	// "expression". If you don't have a field of type ValueExpression in your
+	// solution, then you should make sure that what you are doing is correct :)
+
+//	@Override
+//	public DomainRepresentation representation() {
+//		return new StringRepresentation(expression);
+//	}
+//
+//	@Override
+//	public AvailableExpressions pushScope(ScopeToken scope) throws SemanticException {
+//		return this;
+//	}
+//
+//	@Override
+//	public AvailableExpressions popScope(ScopeToken scope) throws SemanticException {
+//		return this;
+//	}
+}
