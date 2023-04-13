dbf0e8f44db358fb952d85d1ece0b307823e95e4
BeatriceQuaglia <63803867+BeatriceQuaglia@users.noreply.github.com>
2023-04-13 15:51:33 +0200

    Lab-2 ExtSignDomain

--- src/main/java/it/unive/scsr/ExtSignDomain.java
+++ src/main/java/it/unive/scsr/ExtSignDomain.java
@@ -314,21 +314,4 @@ public class ExtSignDomain extends BaseNonRelationalValueDomain<ExtSignDomain> {
 	// of constant fields, and you can change the logic in representation()
 	// accordingly. the only constraint is that the strings used to represent
 	// the elements stay the same (0, +, -, 0+, 0-)
-
-//	@Override
-//	public DomainRepresentation representation() {
-//		if (this == TOP)
-//			return Lattice.topRepresentation();
-//		if (this == BOTTOM)
-//			return Lattice.bottomRepresentation();
-//		if (this == POSITIVE)
-//			return new StringRepresentation("+");
-//		if (this == NEGATIVE)
-//			return new StringRepresentation("-");
-//		if (this == ZERO)
-//			return new StringRepresentation("0");
-//		if (this == ZERO_OR_POSITIVE)
-//			return new StringRepresentation("0+");
-//		return new StringRepresentation("0-");
-//	}
 }
